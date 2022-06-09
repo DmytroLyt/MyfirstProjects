4e2f9c7522026a99defc5f0862b18a9095fb32fd
Dmytro Lytvynenko <105833335+DmytroLyt@users.noreply.github.com>
2022-06-08 22:26:04 -0500

    Update Guessing_Random_number.py

--- Guessing_Random_number.py
+++ Guessing_Random_number.py
@@ -6,13 +6,13 @@ attempt = 5
 while attempt != 0:
     while True:
         try:
-            user_input = int(input('Enter number between 1 and 30: '))
+            user_input = int(input('Enter number between 0 and 30: '))
         except ValueError:
             print("Invalid value, please enter a digits")
             continue
         else:
             break
-    if user_input not in range(0, 30):
+    if user_input not in range(0, 31):
         print("Your number is out of range. Please try again")
         continue
     if user_input > num:
