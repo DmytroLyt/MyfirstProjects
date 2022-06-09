aa1dcf6d4a2c32a9f019e9090f727cab44d4eef6
Dmytro Lytvynenko <105833335+DmytroSwitch@users.noreply.github.com>
2022-06-08 22:13:49 -0500

    Update Guessing_Random_number.py

--- Guessing_Random_number.py
+++ Guessing_Random_number.py
@@ -1,19 +1,27 @@
 import random
 
-num = random.randint(0,50)
+num = random.randint(0, 30)
 
-attempt = 10
-while attempt!=0:
-	user_input=int(input('Enter number between 1 and 50: '))
-	
-	if user_input>num:
-		print (f'Try again, your number is higher! {attempt-1} attempt left')
-	elif user_input<num:
-		print (f'Try again, your number is lower! {attempt-1} attempt left')
-	elif user_input ==num:
-		print('You won!I wished number '+str(num) )
-		break
-	attempt-=1
+attempt = 5
+while attempt != 0:
+    while True:
+        try:
+            user_input = int(input('Enter number between 1 and 30: '))
+        except ValueError:
+            print("Invalid value, please enter a digits")
+            continue
+        else:
+            break
+    if user_input not in range(0, 30):
+        print("Your number is out of range. Please try again")
+        continue
+    if user_input > num:
+        print(f'Try again, your number is higher! {attempt - 1} attempt left')
+    elif user_input < num:
+        print(f'Try again, your number is lower! {attempt - 1} attempt left')
+    elif user_input == num:
+        print('You won!I wished number ' + str(num))
+        break
+    attempt -= 1
 else:
-	print ('Game is over!')
-	
+    print('Game is over!')
